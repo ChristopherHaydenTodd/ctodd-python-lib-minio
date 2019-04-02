439d6d649e847fafc42bc1585f6891f0c16b1193
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-04-02 10:09:49 -0400

    adding empty library with general helprs for later

---
+++ minio_helpers/minio_general_helpers.py
@@ -0,0 +1,16 @@
+"""
+    Purpose:
+        Minio Object Storage General Helpers.
+
+        This library is used to interact with Minio object storage.
+"""
+
+# Python Library Imports
+import logging
+import minio
+
+
+###
+# General Helpers
+###
+
